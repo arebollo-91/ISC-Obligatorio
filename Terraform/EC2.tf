2cf562ca266eac503ed0cc3406e774a808ccd268
Alvaro Rebollo <alvaro.rebollo91@gmail.com>
2025-12-02 20:31:16 +0000

    Se agrega ASG

--- Terraform/EC2.tf
+++ Terraform/EC2.tf
@@ -1,3 +1,93 @@
+#Template para instancias web - ASG
+
+resource "aws_launch_template" "web_lt" {
+  name_prefix   = "isc-web-lt-"
+  image_id      = "ami-0fa3fe0fa7920f68e"
+  instance_type = "t3.micro"
+
+  key_name = "vockey"
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  user_data = base64encode(<<-EOT
+    #!/bin/bash
+    dnf update -y
+
+    # Instalar Docker
+    dnf install -y docker
+    systemctl enable docker
+    systemctl start docker
+
+    # (Opcional) dar permisos a ec2-user
+    usermod -aG docker ec2-user
+
+    # Levantar contenedor web en puerto 80
+    docker rm -f webapp 2>/dev/null || true
+
+    docker run -d \
+      --name webapp \
+      -p 80:80 \
+      nginx
+  EOT
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name     = "isc-web-asg"
+      Proyecto = "ISC-Obligatorio"
+      Rol      = "web"
+    }
+  }
+}
+
+#Autoscaling
+resource "aws_autoscaling_group" "web_asg" {
+  name                      = "isc-web-asg"
+  min_size                  = 2
+  max_size                  = 4
+  desired_capacity          = 2
+  health_check_grace_period = 60
+  health_check_type         = "ELB"
+
+  # Subredes publicas en distintas AZ
+  vpc_zone_identifier = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+
+  # Registrar instancias del ASG en el mismo Target Group del ALB
+  target_group_arns = [
+    aws_lb_target_group.web_tg.arn
+  ]
+
+  launch_template {
+    id      = aws_launch_template.web_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "isc-web-asg"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Proyecto"
+    value               = "ISC-Obligatorio"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Rol"
+    value               = "web"
+    propagate_at_launch = true
+  }
+}
+
 #Web1 instance
 
 resource "aws_instance" "web1" {
